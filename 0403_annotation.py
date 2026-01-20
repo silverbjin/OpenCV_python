a78982e10bb3c7aca499e01a3c44b507bd2c6959
Jinho Bae <silverbjin@gmail.com>
2026-01-20 12:33:40 +0900

    Enhance 0403_annotation.py with explanatory comments
    Added comments to clarify image processing steps.

--- 0403_annotation.py
+++ 0403_annotation.py
@@ -3,15 +3,16 @@ import cv2
 ##import numpy as np
 
 img = cv2.imread('./data/lena.jpg', cv2.IMREAD_GRAYSCALE)
-img[100, 200] = 0  
-print(img[100:110, 200:210]) 
+img[100, 200] = 0              # indexing
+print(img[100:110, 200:210])   # slicing
 
 ##for y in range(100, 400):
 ##    for x in range(200, 300):
 ##        img[y, x] = 0
 
-img[100:400, 200:300] = 0 
+img[100:400, 200:300] = 0      # broadcasting
 
 cv2.imshow('img', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
